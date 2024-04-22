129b427dfaa9008041c3ba1a8e792c0ec327a997
Hadar <hadar5445@gmail.com>
2024-04-22 12:19:58 +0300

    adding calendar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {RefreshControl, SafeAreaView, ScrollView, Button, StyleSheet, Text, View, FlatList} from 'react-native';
+import {RefreshControl, ScrollView, Button, StyleSheet, Text, View, FlatList} from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 const Item = ({ title }) => (
     <View style={styles.item}>
@@ -10,6 +11,7 @@ const Item = ({ title }) => (
 export default function App() {
     const [refreshing, setRefreshing] = React.useState(false);
     const [events, setEvents] = React.useState([]);
+    const [selected, setSelected] = React.useState('');
 
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
@@ -25,7 +27,16 @@ export default function App() {
                 refreshControl={
                     <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
                 }>
-                <Text>Open up App.js to start working on your app!</Text>
+                <Text>לו"ז מדור פיתוח</Text>
+                <Calendar
+                    onDayPress={day => {
+                        setSelected(day.dateString);
+                    }}
+                    markedDates={{
+                        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+                    }}
+                    enableSwipeMonths={true}
+                />
                 <Button onPress={() => setEvents([...events, {title: 'New Item!', id: (events.length + 1).toString()}])} title={'Add new Event'}/>
                 <FlatList
                     data={events}
