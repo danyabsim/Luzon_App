2dc20ce9763861f9ceb26f1f9dfdf80e141e9b62
Hadar <hadar5445@gmail.com>
2024-05-09 14:09:09 +0300

    fixing a little bug that require didn't want to approve

--- constants/AppStyles.ts
+++ constants/AppStyles.ts
@@ -1,6 +1,6 @@
 import {Platform} from "react-native";
 
-export function styleByTime(daySetting: string, nightSetting: string): string {
+export function styleByTime(daySetting: any, nightSetting: any): any {
     const currentHour = new Date().getHours();
     return  (currentHour > 18 && currentHour < 6) ? nightSetting : daySetting;
 }
--- screens/Profile/ProfileScreen.tsx
+++ screens/Profile/ProfileScreen.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import {Image, View, Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {styleByTime} from "../../constants/AppStyles";
 import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
 import {styles} from "./styles";
-// @ts-ignore
-import BlackUserImage from '../../assets/user-_black_.png';
-// @ts-ignore
-import WhiteUserImage from '../../assets/user-_white_.png';
 
 export default function SettingsScreen() {
     const username = useSelector((state: RootState) => state.user.username);
@@ -15,10 +11,15 @@ export default function SettingsScreen() {
 
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.imageHeader}
-                source={require(styleByTime(BlackUserImage, WhiteUserImage))}
-            />
+            {styleByTime(
+                <Image
+                    style={styles.imageHeader}
+                    source={require('../../assets/user-_black_.png')}
+                />,
+                <Image
+                    style={styles.imageHeader}
+                    source={require('../../assets/user-_white_.png')}
+                />)}
             <Text style={styles.text}>{username}</Text>
             <Text style={styles.text}>{isAdmin ? 'Admin' : 'Regular User'}</Text>
         </View>
