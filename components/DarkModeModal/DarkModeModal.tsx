6aca95a03e3d948e2d0f0a69b338350505cf21c1
Hadar <hadar5445@gmail.com>
2024-05-12 15:54:36 +0300

    adding modal components for each one of the options.

---
+++ components/DarkModeModal/DarkModeModal.tsx
@@ -0,0 +1,9 @@
+import {View} from "react-native";
+
+export function DarkModeModal() {
+    return (
+        <View>
+
+        </View>
+    );
+}
--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -2,33 +2,54 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from "./styles";
 import {ModalApp} from "../../components/ModalApp/ModalApp";
+import {ChangePasswordModal} from "../../components/ChangePasswordModal/ChangePasswordModal";
+import {AddUserModal} from "../../components/AddUserModal/AddUserModal";
+import {RemoveUserModal} from "../../components/RemoveUserModal/RemoveUserModal";
+import {DarkModeModal} from "../../components/DarkModeModal/DarkModeModal";
 
 export default function Settings() {
     const isAdmin = true;
     const [modalVisible, setModalVisible] = React.useState(false);
+    const [whoIsOn, setWhoIsOn] = React.useState('');
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.settingItem} onPress={() => setModalVisible(true)}>
+            <TouchableOpacity style={styles.settingItem} onPress={() => {
+                setWhoIsOn('Change Password');
+                setModalVisible(true);
+            }}>
                 <Text style={styles.settingText}>Change Password</Text>
             </TouchableOpacity>
             {isAdmin && (
                 <>
-                    <TouchableOpacity style={styles.settingItem} onPress={() => setModalVisible(true)}>
+                    <TouchableOpacity style={styles.settingItem} onPress={() => {
+                        setWhoIsOn('Add User');
+                        setModalVisible(true);
+                    }}>
                         <Text style={styles.settingText}>Add User</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.settingItem} onPress={() => setModalVisible(true)}>
+                    <TouchableOpacity style={styles.settingItem} onPress={() => {
+                        setWhoIsOn('Remove User');
+                        setModalVisible(true);
+                    }}>
                         <Text style={styles.settingText}>Remove User</Text>
                     </TouchableOpacity>
                 </>
             )}
-            <TouchableOpacity style={styles.settingItem} onPress={() => setModalVisible(true)}>
+            <TouchableOpacity style={styles.settingItem} onPress={() => {
+                setWhoIsOn('Dark Mode');
+                setModalVisible(true);
+            }}>
                 <Text style={styles.settingText}>Dark Mode</Text>
             </TouchableOpacity>
             <ModalApp modalVisible={modalVisible} setModalVisible={setModalVisible} children={
                 <View>
+                    {whoIsOn === 'Change Password' && <ChangePasswordModal/>}
+                    {whoIsOn === 'Add User' && <AddUserModal/>}
+                    {whoIsOn === 'Remove User' && <RemoveUserModal/>}
+                    {whoIsOn === 'Dark Mode' && <DarkModeModal/>}
                     <TouchableOpacity style={styles.button} onPress={() => setModalVisible(false)}>
-                        <Text style={styles.textStyle}>Click Me!</Text>
+                        <Text style={styles.textStyle}>Close</Text>
                     </TouchableOpacity>
                 </View>
             }/>
