a76c464cc3e8d5b0e4e247c48a18135624434c23
Hadar <hadar5445@gmail.com>
2024-08-19 10:14:41 +0300

    making the function more clear

--- constants/AppStyles.ts
+++ constants/AppStyles.ts
@@ -1,10 +1,10 @@
 import {Platform} from "react-native";
 
-export function styleByTime(daySetting: any, nightSetting: any, mode: 'dark' | 'light' | 'auto'): any {
-    if (mode === 'dark') return nightSetting;
-    if (mode === 'light') return daySetting;
+export function styleByTime(lightSetting: any, darkSetting: any, mode: 'dark' | 'light' | 'auto'): any {
+    if (mode === 'dark') return darkSetting;
+    if (mode === 'light') return lightSetting;
     const currentHour = new Date().getHours();
-    return (currentHour > 18 && currentHour < 6) ? nightSetting : daySetting;
+    return (currentHour > 18 && currentHour < 6) ? darkSetting : lightSetting;
 }
 
 export function styleByOS() {
