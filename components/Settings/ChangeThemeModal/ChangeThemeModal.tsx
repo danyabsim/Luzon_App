68becdfaacc3eb2651d41519270426740909af8a
Hadar <hadar5445@gmail.com>
2024-09-01 10:35:42 +0300

    update the theme modal

--- components/Settings/ChangeThemeModal/ChangeThemeModal.tsx
+++ components/Settings/ChangeThemeModal/ChangeThemeModal.tsx
@@ -5,16 +5,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useDispatch, useSelector} from "react-redux";
 import {setDarkMode} from "../../../redux/Theme/themeSlice";
 import {RootState} from "../../../redux/store";
-import {XHR} from "../../../utils/XHR";
 import {setUser} from "../../../redux/User/userSlice";
 import {useTranslation} from "react-i18next";
 import {ButtonApp} from "../../ButtonApp";
 
 export function ChangeThemeModal({onClose}: { onClose: () => void }) {
-    const [mode, setMode] = useState(useSelector((state: RootState) => state.theme.mode));
+    const mode = useSelector((state: RootState) => state.theme.mode);
     const [tempMode, setTempMode] = useState(mode);
     const dispatch = useDispatch();
-    const user = useSelector((state: RootState) => state.user);
     const {t} = useTranslation();
 
     const handleThemeSelection = async (theme: typeof mode) => {
@@ -23,14 +21,8 @@ export function ChangeThemeModal({onClose}: { onClose: () => void }) {
 
     const handleSaveThemeSelection = async () => {
         dispatch(setDarkMode(tempMode));
-        setMode(tempMode);
         await AsyncStorage.setItem('darkMode', JSON.stringify(tempMode));
         console.log(`Selected theme: ${tempMode}`);
-        const storedRememberMe = await AsyncStorage.getItem('rememberMe');
-        XHR(dispatch, '/logout', {
-            username: user.username,
-            isRememberMeOn: storedRememberMe ? storedRememberMe : false
-        });
         dispatch(setUser({username: '', password: ''}));
         DevSettings.reload();
     }
@@ -39,10 +31,10 @@ export function ChangeThemeModal({onClose}: { onClose: () => void }) {
         <View style={styles(mode).container}>
             <Text style={styles(mode).title}>{t('SettingsCT')}</Text>
             <Text style={styles(mode).warningTitle}>{t('ChangeThemeWarning')}</Text>
-            {['light', 'dark'].map(innerMode => (
+            {["light", "dark"].map(innerMode => (
                 <ButtonApp label={t(innerMode.charAt(0).toUpperCase() + innerMode.slice(1))} key={innerMode}
                            buttonStyle={[styles(mode).optionButton, {
-                               backgroundColor: innerMode === tempMode ? '#007bff' : '#98a2b7',
+                               backgroundColor: innerMode == tempMode ? '#007bff' : '#98a2b7',
                            }]} onPress={() => handleThemeSelection(innerMode as typeof mode)}
                 />
             ))}
