4a2d3bb7c555c18c2e3422db9215406cb77719f8
Hadar <hadar5445@gmail.com>
2024-09-05 14:18:55 +0300

    as we removed /logout from the server it is not necessary anymore

--- components/DrawerContainer/DrawerContainer.tsx
+++ components/DrawerContainer/DrawerContainer.tsx
@@ -5,15 +5,12 @@ import {styleByTime} from "../../utils/AppStyles";
 import {useDispatch, useSelector} from "react-redux";
 import {setUser} from "../../redux/User/userSlice";
 import {RootState} from "../../redux/store";
-import {XHR} from "../../utils/XHR";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import {useTranslation} from "react-i18next";
 
 export function DrawerContainer({navigation}: any) {
     const dispatch = useDispatch();
     const mode = useSelector((state: RootState) => state.theme.mode);
     const image = useSelector((state: RootState) => state.user.image);
-    const user = useSelector((state: RootState) => state.user);
     const {t} = useTranslation();
 
     return (
@@ -47,11 +44,6 @@ export function DrawerContainer({navigation}: any) {
                     title={t("Logout")}
                     source={styleByTime(require('../../assets/logout (black).png'), require('../../assets/logout (white).png'), mode)}
                     onPress={async () => {
-                        const storedRememberMe = await AsyncStorage.getItem('rememberMe');
-                        XHR(dispatch, '/logout', {
-                            username: user.username,
-                            isRememberMeOn: storedRememberMe ? storedRememberMe : false
-                        });
                         dispatch(setUser({username: '', password: ''}));
                         navigation.navigate('Home');
                         navigation.closeDrawer();
