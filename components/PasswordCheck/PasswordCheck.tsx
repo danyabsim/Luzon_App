32e701206b4c9bf4ed179667a4444d40ce169b67
Hadar <hadar5445@gmail.com>
2024-10-30 14:07:28 +0200

    making the password check more simple and changeable...

--- components/PasswordCheck/PasswordCheck.tsx
+++ components/PasswordCheck/PasswordCheck.tsx
@@ -1,45 +1,38 @@
-import {IPasswordCheckProps} from "./IPasswordCheckProps";
-import {View, Text} from "react-native";
-import {useSelector} from "react-redux";
-import {RootState} from "../../redux/store";
-import {styles} from "./styles";
-import {useEffect} from "react";
-import {useTranslation} from "react-i18next";
+import { IPasswordCheckProps } from "./IPasswordCheckProps";
+import { View, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { styles } from "./styles";
+import { useEffect, useMemo } from "react";
+import { useTranslation } from "react-i18next";
 
-export function PasswordCheck(props: IPasswordCheckProps) {
+export function PasswordCheck({ password, setPassedCheck }: IPasswordCheckProps) {
     const mode = useSelector((state: RootState) => state.theme.mode);
-    const {t} = useTranslation();
+    const { t, i18n } = useTranslation();
 
-    function checkCharacters() {
-        return props.password.length < 8;
-    }
-
-    function checkEnglishLetter() {
-        return !/[a-zA-Z]/.test(props.password);
-    }
-
-    function checkNumber() {
-        return !/\d/.test(props.password);
-    }
+    const checks = useMemo(() => ({
+        length: password.length >= 8,
+        letter: /[a-zA-Z]/.test(password),
+        number: /\d/.test(password)
+    }), [password]);
 
     useEffect(() => {
-        props.setPassedCheck(!checkCharacters() && !checkEnglishLetter() && !checkNumber());
-    }, [props.password]);
+        setPassedCheck(Object.values(checks).every(Boolean));
+    }, [checks]);
 
     return (
         <View>
-            <View style={styles(mode).rowContainer}>
-                <View style={checkCharacters() ? styles(mode).emptyBox : styles(mode).fullBox}/>
-                <Text style={styles(mode).conditionText}>{t('PasswordCheckLength')}</Text>
-            </View>
-            <View style={styles(mode).rowContainer}>
-                <View style={checkEnglishLetter() ? styles(mode).emptyBox : styles(mode).fullBox}/>
-                <Text style={styles(mode).conditionText}>{t('PasswordCheckLetter')}</Text>
-            </View>
-            <View style={styles(mode).rowContainer}>
-                <View style={checkNumber() ? styles(mode).emptyBox : styles(mode).fullBox}/>
-                <Text style={styles(mode).conditionText}>{t('PasswordCheckNumber')}</Text>
-            </View>
+            {[
+                { check: checks.length, label: t('PasswordCheckLength') },
+                { check: checks.letter, label: t('PasswordCheckLetter') },
+                { check: checks.number, label: t('PasswordCheckNumber') }
+            ].map(({ check, label }, index) => (
+                <View key={index} style={styles(mode).rowContainer}>
+                    {i18n.language == 'he' && <Text style={styles(mode).conditionText}>{label}</Text>}
+                    <View style={check ? styles(mode).fullBox : styles(mode).emptyBox} />
+                    {i18n.language == 'en' && <Text style={styles(mode).conditionText}>{label}</Text>}
+                </View>
+            ))}
         </View>
     );
 }
