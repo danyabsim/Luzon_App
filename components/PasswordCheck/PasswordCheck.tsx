43797a1334917d44e3dba3b5da2810771cd87c19
Hadar <hadar5445@gmail.com>
2024-10-30 14:07:54 +0200

    cleanup in PasswordCheck

--- components/PasswordCheck/PasswordCheck.tsx
+++ components/PasswordCheck/PasswordCheck.tsx
@@ -1,14 +1,14 @@
-import { IPasswordCheckProps } from "./IPasswordCheckProps";
-import { View, Text } from "react-native";
-import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
-import { styles } from "./styles";
-import { useEffect, useMemo } from "react";
-import { useTranslation } from "react-i18next";
+import {IPasswordCheckProps} from "./IPasswordCheckProps";
+import {Text, View} from "react-native";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
+import {styles} from "./styles";
+import {useEffect, useMemo} from "react";
+import {useTranslation} from "react-i18next";
 
-export function PasswordCheck({ password, setPassedCheck }: IPasswordCheckProps) {
+export function PasswordCheck({password, setPassedCheck}: IPasswordCheckProps) {
     const mode = useSelector((state: RootState) => state.theme.mode);
-    const { t, i18n } = useTranslation();
+    const {t, i18n} = useTranslation();
 
     const checks = useMemo(() => ({
         length: password.length >= 8,
@@ -23,13 +23,13 @@ export function PasswordCheck({ password, setPassedCheck }: IPasswordCheckProps)
     return (
         <View>
             {[
-                { check: checks.length, label: t('PasswordCheckLength') },
-                { check: checks.letter, label: t('PasswordCheckLetter') },
-                { check: checks.number, label: t('PasswordCheckNumber') }
-            ].map(({ check, label }, index) => (
+                {check: checks.length, label: t('PasswordCheckLength')},
+                {check: checks.letter, label: t('PasswordCheckLetter')},
+                {check: checks.number, label: t('PasswordCheckNumber')}
+            ].map(({check, label}, index) => (
                 <View key={index} style={styles(mode).rowContainer}>
                     {i18n.language == 'he' && <Text style={styles(mode).conditionText}>{label}</Text>}
-                    <View style={check ? styles(mode).fullBox : styles(mode).emptyBox} />
+                    <View style={check ? styles(mode).fullBox : styles(mode).emptyBox}/>
                     {i18n.language == 'en' && <Text style={styles(mode).conditionText}>{label}</Text>}
                 </View>
             ))}
